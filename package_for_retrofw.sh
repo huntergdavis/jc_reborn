f8d7caba0a14819a52350d1cf767720e4973d3b3
huntergdavis <huntergdavis@penguin.lxd>
2021-07-12 23:38:21 -0500

    adding packager for ipkg

---
+++ package_for_retrofw.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p bin
+cp jc_reborn ipkg/home/retrofw/apps/johnny/jc_reborn
+cp -R res ipkg/home/retrofw/apps/johnny/
+cd ipkg
+
+tar -czvf control.tar.gz control
+tar -czvf data.tar.gz home
+ar rv johnny.ipk control.tar.gz data.tar.gz debian-binary
+
+cd ..
+
+mv ipkg/johnny.ipk bin/
